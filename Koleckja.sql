786c07ac972acd765da5bf7b60c7de27d5eb162b
hickkyp <128061853+HenriqueFernande@users.noreply.github.com>
2024-10-08 08:49:56 -0300

    Add files via upload

---
+++ Koleckja.sql
@@ -0,0 +1,19 @@
+-- TABLE
+CREATE TABLE cliente(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome TEXT,
+  cpf TEXT,
+  endereco TEXT,
+  data_nascimento DATE,
+  email TEXT
+  numero_telefone INTEGER,
+  genero TEXT
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
